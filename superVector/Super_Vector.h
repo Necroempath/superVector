fc32be54d07354dcc85c3e617c3c0e8d4e890b86
Nate <8bitsodagamer@gmail.com>
2025-03-16 10:47:05 +0400

    template class and init_list update

--- superVector/Super_Vector.h
+++ superVector/Super_Vector.h
@@ -1,74 +1,345 @@
 #pragma once
-#include <ostream>
+#include <iostream>
+#include <initializer_list>
+
+template <typename T>
 class SuperVector
 {
 private:
-    float* _data;
+    T* _data;
     size_t _size;
     size_t _capacity;
 
-    void ResizeArray(const float factor);
+    void ResizeArray(const T factor)
+    {
+        T* temp = new T[_capacity * factor]();
+
+        for (size_t i = 0; i < _size; i++)
+        {
+            temp[i] = _data[i];
+        }
+
+        _capacity *= factor;
+        delete[] _data;
+        _data = temp;
+    }
 
 public:
-    SuperVector();
+    SuperVector() : _data(nullptr), _size(0), _capacity(0) {}
 
-    SuperVector(size_t size);
+    SuperVector(size_t size) : _size(size), _capacity(size * 1.5f)
+    {
+        _data = new T[_capacity] {};
+    }
 
-    SuperVector(const SuperVector& other);
+    SuperVector(const SuperVector& other) : _size(other._size), _capacity(other._capacity)
+    {
+        _data = new T[_capacity];
 
-    SuperVector(SuperVector&& other) noexcept;
+        for (size_t i = 0; i < _size; i++)
+        {
+            _data[i] = other._data[i];
+        }
+    }
 
-    SuperVector& operator=(const SuperVector& vector);
+    SuperVector(SuperVector&& other) noexcept : _data(other._data), _size(other._size), _capacity(other._capacity)
+    {
+        other._data = nullptr;
+        other._size = 0;
+        other._capacity = 0;
+    }
 
-    SuperVector& operator=(SuperVector&& vector) noexcept;
+    SuperVector(std::initializer_list<T> init_list) : _size(init_list.size()), _capacity(init_list.size() * 1.5f)
+    {
+        _data = new T[_capacity];
+        size_t index = 0;
 
-    SuperVector operator+(const SuperVector& other) const;
+        for (auto value : init_list)
+        {
+            _data[index++] = value;
+        }
+    }
 
-    SuperVector operator-(const SuperVector& other) const;
+    inline size_t Size() const
+    {
+        return _size;
+    }
 
-    SuperVector operator*(const SuperVector& other) const;
+    T& operator[](size_t index)
+    {
+        if (index >= 0 && index < _size)
+            return _data[index];
+    }
 
-    SuperVector operator*(const float& scalar) const;
+    const T operator[](size_t index) const
+    {
+        if (index >= 0 && index < _size)
+            return _data[index];
+    }
 
-    SuperVector operator/(const SuperVector& other) const;
+    SuperVector operator/(const SuperVector& other) const
+    {
+        size_t size = _size < other.Size() ? _size : other.Size();
 
-    SuperVector operator/(const float& scalar) const;
+        SuperVector vector(size);
 
-    SuperVector& operator+=(const SuperVector& other);
+        for (size_t i = 0; i < size; i++)
+        {
+            if (vector._data[i] == 0)
+                return *this;
+        }
 
-    SuperVector& operator-=(const SuperVector& other);
+        for (size_t i = 0; i < size; i++)
+        {
+            vector._data[i] = _data[i] / other._data[i];
+        }
 
-    SuperVector& operator*=(const float& scalar);
+        return vector;
+    }
 
-    SuperVector& operator/=(const float& scalar);
+    SuperVector operator/(const T& scalar) const
+    {
+        if (scalar == 0) return *this;
 
-    friend std::ostream& operator<<(std::ostream& os, const SuperVector& vector);
+        SuperVector vector(Size());
 
-    friend std::istream& operator>>(std::istream& is, SuperVector& vector);
+        for (size_t i = 0; i < Size(); i++)
+        {
+            vector._data[i] = _data[i] / scalar;
+        }
 
-    SuperVector& operator++();
+        return vector;
+    }
 
-    SuperVector operator++(int);
+    SuperVector operator*(const SuperVector& other) const
+    {
+        size_t size = _size < other.Size() ? _size : other.Size();
 
-    SuperVector& operator--();
+        SuperVector vector(size);
 
-    SuperVector operator--(int);
+        for (size_t i = 0; i < size; i++)
+        {
+            vector._data[i] = _data[i] * other._data[i];
+        }
 
-    const float operator[](int index) const;
+        return vector;
+    }
 
-    float& operator[](int index);
+    SuperVector operator*(const T& scalar) const
+    {
+        SuperVector vector(Size());
 
-    size_t Size() const;
+        for (size_t i = 0; i < Size(); i++)
+        {
+            vector._data[i] = _data[i] * scalar;
+        }
 
-    void PushBack(float value);
+        return vector;
+    }
 
-    bool Insert(int index, float value);
+    SuperVector operator-(const SuperVector& other) const
+    {
+        size_t size = _size < other.Size() ? _size : other.Size();
 
-    void Pop();
+        SuperVector vector(size);
 
-    bool Remove(int index);
+        for (size_t i = 0; i < size; i++)
+        {
+            vector._data[i] = _data[i] - other._data[i];
+        }
 
-    ~SuperVector();
+        return vector;
+    }
+
+    SuperVector operator+(const SuperVector& other) const
+    {
+        size_t size = _size < other.Size() ? _size : other.Size();
+
+        SuperVector vector(size);
+
+        for (size_t i = 0; i < size; i++)
+        {
+            vector._data[i] = _data[i] + other._data[i];
+        }
+
+        return vector;
+    }
+
+    SuperVector& operator=(SuperVector&& vector) noexcept
+    {
+        if (&vector == this) return *this;
+
+        _size = vector._size;
+        _capacity = vector._capacity;
+        _data = vector._data;
+
+        vector._size = 0;
+        vector._capacity = 0;
+        vector._data = nullptr;
+
+        return *this;
+    }
+
+    SuperVector& operator=(const SuperVector& vector)
+    {
+        if (&vector == this) return *this;
+
+        _size = vector._size;
+        _capacity = vector._capacity;
+        _data = new float[_capacity];
+
+        for (size_t i = 0; i < _size; i++)
+        {
+            _data[i] = vector._data[i];
+        }
+
+        return *this;
+    }
+
+    SuperVector& operator+=(const SuperVector& other)
+    {
+        *this = *this + other;
+
+        return *this;
+    }
+
+    SuperVector& operator-=(const SuperVector& other)
+    {
+        *this = *this - other;
+
+        return *this;
+    }
+
+    SuperVector& operator*=(const float& scalar)
+    {
+        *this = *this * scalar;
+
+        return *this;
+    }
+
+    SuperVector& operator/=(const float& scalar)
+    {
+        *this = *this / scalar;
+
+        return *this;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const SuperVector& vector)
+    {
+        os << "[";
+        for (size_t i = 0; i < vector.Size(); i++)
+        {
+            os << vector[i];
+
+            if (i < vector.Size() - 1) os << ", ";
+        }
+        os << "]";
+
+        return os;
+    }
+
+    friend std::istream& operator>>(std::istream& is, SuperVector& vector)
+    {
+        for (size_t i = 0; i < vector.Size(); i++)
+        {
+            is >> vector[i];
+        }
+
+        return is;
+    }
+
+    SuperVector& operator++()
+    {
+        for (size_t i = 0; i < Size(); i++)
+        {
+            _data[i]++;
+        }
+
+        return *this;
+    }
+
+    SuperVector operator++(int)
+    {
+        SuperVector temp = *this;
+
+        for (size_t i = 0; i < Size(); i++)
+        {
+            _data[i]++;
+        }
+
+        return temp;
+    }
+
+    SuperVector& operator--()
+    {
+        for (size_t i = 0; i < Size(); i++)
+        {
+            _data[i]--;
+        }
+
+        return *this;
+    }
+
+    SuperVector operator--(int)
+    {
+        SuperVector temp = *this;
+
+        for (size_t i = 0; i < Size(); i++)
+        {
+            _data[i]--;
+        }
+
+        return temp;
+    }
+
+    void PushBack(T value)
+    {
+        if (_size == _capacity) ResizeArray(1.5f);
+
+        _data[_size++] = value;
+    }
+
+    bool Insert(size_t index, T value)
+    {
+        if (index < 0 || index > _size) return 0;
+
+        if (_size == _capacity) ResizeArray(1.5f);
+
+        for (size_t i = _size; i > index; i--)
+        {
+            _data[i] = _data[i - 1];
+        }
+
+        _data[index] = value;
+
+        _size++;
+        return 1;
+    }
+
+    void Pop()
+    {
+        _data[--_size] = 0;
+
+        if (_size <= _capacity * 0.7f) ResizeArray(0.7f);
+    }
+
+    bool Remove(size_t index)
+    {
+        if (index < 0 || index >= _size) return 0;
+
+        for (size_t i = index; i < _size - 1; i++)
+        {
+            _data[i] = _data[i + 1];
+        }
+
+        Pop();
+        return true;
+    }
+
+    ~SuperVector()
+    {
+        delete[] _data;
+    }
 
 };
 
--- superVector/main.cpp
+++ superVector/main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include "Super_Vector.h"
 
-void DisplayVector(const SuperVector& vector)
+void DisplayVector(const SuperVector<float>& vector)
 {
     for (int i = 0; i < vector.Size(); ++i)
     {
@@ -12,27 +12,14 @@ void DisplayVector(const SuperVector& vector)
 
 int main()
 {
-    SuperVector vector(5);
-    SuperVector vector_1(7);
-
-    vector_1[0] = 9;
-    vector_1[1] = 3;
-    vector_1[2] = 4;
-    vector_1[3] = -1;
-    vector_1[4] = 8;
-    vector[0] = 10;
-    vector[1] = 6;
-    vector[2] = 7;
-    vector[3] = 0;
-    vector[4] = -11;
-
+    SuperVector<double> vector = {1,2,3,4,5};
+    SuperVector<short> vector_1(5);
     
- /*   vector_1--;*/
     std::cout << vector++ << "\n";
-    vector.PushBack(7.1);
+    vector.PushBack(17.2);
     std::cout << ++vector << "\n";
     vector.Remove(1);
     std::cout << vector;
-    //CANNOT OVERLOAD ISTREAM>>
+
     return 0;
 }
